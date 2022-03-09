5499c915b16749fdf559829ff2614c7a9fad62f8
yakisyst3m <98212776+yakisyst3m@users.noreply.github.com>
2022-03-09 19:49:33 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,16 +4,17 @@
 
 utilisateur=$(grep 1000 /etc/passwd | awk -F ":" '{print $1}')
 uidutilisateur=$(echo $UID)
+cheminInstall="/home/$utilisateur/Documents/Linux-Post_Install/"
 if [ "$uidutilisateur" = "0" ] ; then
-    if [ ! -d "/home/$utilisateur/Documents/Linux-Post_Install/" ] ; then
-        mkdir /home/$utilisateur/Documents/Linux-Post_Install/
-        cp -r * /home/$utilisateur/Documents/Linux-Post_Install/
-        cd /home/$utilisateur/Documents/Linux-Post_Install/
+    if [ ! -d "$cheminInstall" ] ; then
+        mkdir $cheminInstall
+        cp -r * $cheminInstall
+        cd $cheminInstall
         chmod -R 750 /home/$utilisateur/Documents/
         chown -R $utilisateur: /home/$utilisateur/Documents/
         ./Post_install-FSICS_PRO.sh
     else
-        cd /home/$utilisateur/Documents/Linux-Post_Install/
+        cd $cheminInstall
         chmod -R 750 /home/$utilisateur/Documents/
         chown -R $utilisateur: /home/$utilisateur/Documents/
         ./Post_install-FSICS_PRO.sh
